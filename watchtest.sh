f73757567a96c2047a4e2d7c816c5fa4dedc27f4
ale-cci <alecrd98@gmail.com>
2020-08-29 23:21:53 +0200

    Remove watchtest.sh

--- watchtest.sh
+++
@@ -1,6 +0,0 @@
-while true
-do
-    echo '--------------------------------------------------------------------------------'
-    inotifywait -qq -r -e create,close_write,modify,move,delete ./ && go test ./...
-
-done
